b775593394020ebfffa322946797b52ee9e2bc3e
pedrokekww <pedroveb119@gmail.com>
2025-02-07 14:10:06 -0300

    Create Instancia.java

---
+++ Instancia.java
@@ -0,0 +1,6 @@
+   public static void main(String[] args) {
+        System.out.println("Área do quadrado (lado 4): " + calcularQuadrado(4));
+        System.out.println("Área do retângulo (largura 5, altura 3): " + calcularRetangulo(5, 3));
+        System.out.println("Área do triângulo (base 6, altura 4): " + calcularTriangulo(6, 4));
+        System.out.println("Área do círculo (raio 3): " + calcularCirculo(3));
+    }
